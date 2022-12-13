82b4ec86fe5bd3ea4c7e3e3e9b2e207502ae1327
yasmindeoliveira <yaz.oliveira01@gmail.com>
2022-12-13 17:04:22 +0100

    completato milestone4

--- GestoreEventi/Program.cs
+++ GestoreEventi/Program.cs
@@ -2,25 +2,52 @@
 using GestoreEventi;
 using GestoreEventi.CustomException;
 
-Evento eventoinserito = new Evento("Conferenza sul clima", "12/20/2022", 150);
-Console.WriteLine(eventoinserito.ToString());
-eventoinserito.PrenotaPosti(120);
-Console.WriteLine(eventoinserito.ToString());
-eventoinserito.DisdiciPosti(10);
-Console.WriteLine(eventoinserito);
+Console.WriteLine("Come si chiama il programma eventi?");
+string nomeProgrammaUtente = Console.ReadLine();
+
+ProgrammaEventi programma = new ProgrammaEventi(nomeProgrammaUtente);
+
+Console.WriteLine("Quanti eventi verrano inseriti?");
+int numeroEventi = int.Parse(Console.ReadLine());
+while(numeroEventi != programma.EventiTotali())
+{
+    try
+    {
+        Console.WriteLine("Inserisci il nome dell'evento:");
+        string titoloUtente = Console.ReadLine();
+        Console.WriteLine("Inserisci la data dell'evento (MM/dd/yyyy):");
+        string dataUtente = Console.ReadLine();
+        Console.WriteLine("Inserisci il numero di posti totali:");
+        int postiUtente = int.Parse(Console.ReadLine());
+
+        Evento eventoUtente = new Evento(titoloUtente, dataUtente, postiUtente);
+        programma.AggiungeEvento(eventoUtente);
+    }
+    catch (ResultCannotBeNegativeException e)
+    {
+        Console.WriteLine(e.Message);
+    }
+    catch (ArgumentNullException e)
+    {
+        Console.WriteLine(e.Message);
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine(e.Message);
+    }
+
+}
+
+Console.WriteLine(programma.EventiTotali());
+Console.WriteLine(programma);
+Console.WriteLine("Inseriisci una data (MM/dd/yyyy):");
+string dataInseritaDaUtente = Console.ReadLine();
+Console.WriteLine(ProgrammaEventi.GetStringaListaEventi(programma.eventiInData(dataInseritaDaUtente)));
+programma.SvuotaLista();
 
 
-Console.WriteLine("Inserisci il nome dell'evento:");
-string titoloUtente = Console.ReadLine();
-Console.WriteLine("Inserisci la data dell'evento (MM/dd/yyyy):");
-string dataUtente = Console.ReadLine();
-Console.WriteLine("Inserisci il numero di posti totali:");
-int postiUtente = int.Parse(Console.ReadLine());
 
-
-Evento eventoUtente = new Evento(titoloUtente, dataUtente, postiUtente);
-
-
+/*
 Console.WriteLine("Quanti posti desideri prenotare?");
 int quantitaPostiUtente = int.Parse(Console.ReadLine());
 eventoUtente.PrenotaPosti(quantitaPostiUtente);
@@ -45,7 +72,7 @@ while(check == "si")
     check = Console.ReadLine();
 }
 
-
+*/
 
 
 
@@ -68,12 +95,13 @@ while(check == "si")
 /*
 try
 {
-    Evento eventoinserito = new Evento("Conferenza sul clima", "12/20/2022", 150);
-    Console.WriteLine(eventoinserito.ToString());
-    eventoinserito.PrenotaPosti(120);
-    Console.WriteLine(eventoinserito.ToString());
-    eventoinserito.DisdiciPosti(10);
-    Console.WriteLine(eventoinserito.ToString());
+
+Evento eventoinserito = new Evento("Conferenza sul clima", "12/20/2022", 150);
+Console.WriteLine(eventoinserito.ToString());
+eventoinserito.PrenotaPosti(120);
+Console.WriteLine(eventoinserito.ToString());
+eventoinserito.DisdiciPosti(10);
+Console.WriteLine(eventoinserito); // Il .ToString è sottinteso
 
     Console.WriteLine("Inserisci il nome dell'evento:");
     Console.ReadLine();
